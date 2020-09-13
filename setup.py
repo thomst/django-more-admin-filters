db6c961116d80ffbfd7c1fae1e4a60be83401488
Thomas Leichtfuß <leichtfuss@systopia.de>
2020-09-13 16:31:24 +0200

    added official support for django 3.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=[
-        "Django>=1.11,<=3.0",
+        "Django>=1.11,<=3.1",
     ],
     classifiers=[
         dev_status,
