271fb87ad3f3a0494883242a868b96ccc20c07d4
Thomas Leichtfuß <leichtfuss@systopia.de>
2025-06-06 13:25:10 +0200

    pypi.org wants to know the long-description format

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     version=version,
     description="Additional filters for django-admin.",
     long_description=read("README.md"),
+    long_description_content_type='text/markdown',
     author="Thomas Leichtfuß",
     author_email="thomas.leichtfuss@posteo.de",
     url="https://github.com/thomst/django-more-admin-filters",
